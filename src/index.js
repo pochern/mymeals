45f8e2c348d99ba784a65b286af2cc46406fcd27
Polina Chernomaz <pachernomaz@gmail.com>
2020-11-21 14:14:44 -0500

    Installed redux

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
+import { createStore } from 'redux'
+import reducer from './reducers'
+
+const store = createStore(reducer)
+
+console.log(store.getState())
 
 ReactDOM.render(
   <React.StrictMode>
