15e91bed8ee397db2cb75febe5d3147977e86be8
Polina Chernomaz <pachernomaz@gmail.com>
2020-11-21 14:19:23 -0500

    Made compatible with Redux DevTools

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import App from './components/App'
 import { createStore } from 'redux'
 import reducer from './reducers'
 
-const store = createStore(reducer)
-
-console.log(store.getState())
+// eslint-disable-next-line no-unused-vars
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(
   <React.StrictMode>
