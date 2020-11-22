9f87c217935b05e9243825208ce9bf05afeef0b9
Polina Chernomaz <pachernomaz@gmail.com>
2020-11-21 23:38:14 -0500

    Mapping redux state to component and putting data in array

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,36 @@
 /* eslint-disable react/no-unescaped-entities */
 import React, { Component } from 'react'
-import { addRecipe } from '../actions'
-import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 
 class App extends Component {
-  static propTypes = {
-    store: PropTypes.object
-  }
-
-  state = {
-    calendar: null
-  }
-
-  componentDidMount () {
-    const { store } = this.props
-
-    store.subscribe(() => {
-      this.setState(() => ({
-        calendar: store.getState()
-      }))
-    })
-  }
-
-  submitFood = () => {
-    this.props.store.dispatch(addRecipe({
-      day: 'monday',
-      meal: 'breakfast',
-      recipe: {
-        label: this.input.value
-      }
-    }))
-
-    this.input.value = ''
-  }
-
   render () {
+    console.log('Props', this.props)
     return (
       <div>
-        <input
-          type='text'
-          ref={(input) => { this.input = input }}
-          placeholder="Monday's Breakfast"
-        />
-        <button onClick={this.submitFood}>Submit</button>
-
-        <pre>
-          Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
-        </pre>
+        Hello World
       </div>
     )
   }
 }
 
-export default App
+// Maps Redux state to components props
+function mapStateToProps (calendar) {
+  // If you want to be able to make a grid in React, you need to use map so you need an array
+  // With Redux, it makes more sense to have DS be an object
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+          ? calendar[day][meal]
+          : null
+
+        return meals
+      }, {})
+    }))
+  }
+}
+
+export default connect(mapStateToProps)(App)
