f1cbcef22a16c082f3fcdc27a4857cb7886c76ce
Polina Chernomaz <pachernomaz@gmail.com>
2020-11-21 20:33:38 -0500

    Passed store prop to App

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './components/App'
 import { createStore } from 'redux'
 import reducer from './reducers'
 
-// eslint-disable-next-line no-unused-vars
 const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
@@ -13,7 +12,7 @@ const store = createStore(
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store={store}/>
   </React.StrictMode>,
   document.getElementById('root')
 )
